698cf329b103546dd53d9c0d292e101f4c0bdd65
avalo-lnx <avalo.rf@gmail.com>
2023-10-20 17:19:32 -0400

    fix: :adhesive_bandage: Fix error with naming of classes

--- src/test/java/me/dio/santanderDevBacked/ApplicationTests.java
+++ src/test/java/me/dio/santanderDevBacked/ApplicationTests.java
@@ -1,10 +1,10 @@
-package me.dio.demo;
+package me.dio.santanderDevBacked;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
